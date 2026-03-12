f8f6e447ed2bc3cc45bfb48114313592ff67b1c6
Darius Mclean <126315489+NotDurris@users.noreply.github.com>
2026-03-12 14:11:55 +1000

    Refectoring skills manager

--- Components/Battle/SkillsManager/skills_manager.gd
+++ Components/Battle/SkillsManager/skills_manager.gd
@@ -15,7 +15,6 @@ const DOT = preload("uid://g8fptfm0a2ey")
 
 var selected_dice : Array[dice]
 var selected_skill : Skill
-var battle_refs : BattleRefs
 
 var uses_left : int :
 	set(value):
@@ -28,58 +27,21 @@ var uses_left : int :
 				dot.set_colour(Dot.GREY_COLOUR)
 
 func _ready() -> void:
-	selection_pop_up_close.pressed.connect(func():
-		selection_pop_up.hide()
-		selected_dice.clear()
-		selected_skill = null
-	)
-	selection_pop_up_confirm.pressed.connect(apply_skill)
+	selection_pop_up_close.pressed.connect(close_selection_pop_up)
+	
 	for i in range(skill_buttons.size()):
 		skill_buttons[i].pressed.connect(func() : skill_selected(i))
 
-func initiate_skills(br : BattleRefs):
-	battle_refs = br
+func close_selection_pop_up():
 	selection_pop_up.hide()
-	
-	for dot in skill_dots:
-		dot.queue_free()
-	skill_dots.clear()
-	
-	for i in range(YourFighter.stats.mental):
-		var new_dot : Dot = DOT.instantiate()
-		skill_dots.append(new_dot)
-		dots_container.add_child(new_dot)
-	
-	for i in range(skill_buttons.size()):
-		var target_button : Button = skill_buttons[i]
-		if i < YourFighter.stats.skills.size() and YourFighter.stats.skills.size() != 0:
-			button_disabled[i] = false
-			target_button.disabled = button_disabled[i]
-			target_button.text = YourFighter.stats.skills[i].name
-		else:
-			button_disabled[i] = true
-			target_button.disabled = button_disabled[i]
-
-func activate(br : BattleRefs):
-	br.your_dice_visual.dice_selected.connect(your_dice_selection)
-	br.opponents_dice_visual.dice_selected.connect(opponent_dice_selection)
-	for i in range(skill_buttons.size()):
-		skill_buttons[i].disabled = button_disabled[i]
-	
-	uses_left = YourFighter.stats.mental
-
-func deactivate(br : BattleRefs):
-	br.your_dice_visual.dice_selected.disconnect(your_dice_selection)
-	br.opponents_dice_visual.dice_selected.disconnect(opponent_dice_selection)
-	for button in skill_buttons:
-		button.disabled = true
+	selected_dice.clear()
+	selected_skill = null
 
 func skill_selected(id : int):
+	selection_pop_up.show()
 	selected_skill = YourFighter.stats.skills[id]
 	
-	selection_pop_up.show()
-	
-	update_reminder()
+	update_reminder_text()
 	
 	match (selected_skill.target_type):
 		Skill.TargetType.NONE:
@@ -90,21 +52,7 @@ func skill_selected(id : int):
 		Skill.TargetType.ALL_DICE:
 			pass
 
-func apply_skill():
-	uses_left -= 1
-	if uses_left == 0:
-		deactivate(battle_refs)
-	var skill_action : SkillAction = selected_skill.skill_action.new()
-	skill_action.do_action(battle_refs, selected_dice)
-	
-	battle_refs.your_dice_visual.update_dice_container(battle_refs.you.available_dice)
-	battle_refs.opponents_dice_visual.update_dice_container(battle_refs.opponent.available_dice)
-	
-	selection_pop_up.hide()
-	selected_skill = null
-	selected_dice.clear()
-
-func update_reminder():
+func update_reminder_text():
 	if selected_skill == null : return
 	
 	var target : String = ""
@@ -126,15 +74,74 @@ func update_reminder():
 		Skill.TargetType.ALL_DICE:
 			selection_pop_up_label.text = "Confirm to use skill"
 
-func your_dice_selection(id : int):
-	handle_dice_selection(id, battle_refs.you.available_dice, Skill.TargetAffiliation.SELF)
+func set_usage_dots():
+	for dot in skill_dots:
+		dot.queue_free()
+	skill_dots.clear()
+	
+	for i in range(YourFighter.stats.mental):
+		var new_dot : Dot = DOT.instantiate()
+		skill_dots.append(new_dot)
+		dots_container.add_child(new_dot)
 
-func opponent_dice_selection(id : int):
-	handle_dice_selection(id, battle_refs.you.available_dice, Skill.TargetAffiliation.SELF)
+func initiate_btns(br : BattleRefs):
+	for connection in selection_pop_up_confirm.pressed.get_connections():
+		selection_pop_up_confirm.pressed.disconnect(connection)
+	
+	selection_pop_up_confirm.pressed.connect(func() : apply_skill(br))
+	br.your_dice_visual.dice_selected.connect(func(value) : your_dice_selection(value, br))
+	br.opponents_dice_visual.dice_selected.connect(func(value) : opponent_dice_selection(value, br))
+
+func initiate_skills(br : BattleRefs):
+	initiate_btns(br)
+	
+	selection_pop_up.hide()
+	
+	set_usage_dots()
+	
+	for i in range(skill_buttons.size()):
+		var target_button : Button = skill_buttons[i]
+		if i < YourFighter.stats.skills.size() and YourFighter.stats.skills.size() != 0:
+			button_disabled[i] = false
+			target_button.disabled = button_disabled[i]
+			target_button.text = YourFighter.stats.skills[i].name
+		else:
+			button_disabled[i] = true
+			target_button.disabled = button_disabled[i]
+
+func activate(_br : BattleRefs):
+	for i in range(skill_buttons.size()):
+		skill_buttons[i].disabled = button_disabled[i]
+	
+	uses_left = YourFighter.stats.mental
+
+func deactivate(_br : BattleRefs):
+	for button in skill_buttons:
+		button.disabled = true
+
+func apply_skill(br : BattleRefs):
+	uses_left -= 1
+	if uses_left == 0:
+		deactivate(br)
+	var skill_action : SkillAction = selected_skill.skill_action.new()
+	skill_action.do_action(br, selected_dice)
+	
+	br.your_dice_visual.update_dice_container(br.you.available_dice)
+	br.opponents_dice_visual.update_dice_container(br.opponent.available_dice)
+	
+	selection_pop_up.hide()
+	selected_skill = null
+	selected_dice.clear()
+
+func your_dice_selection(id : int, br : BattleRefs):
+	handle_dice_selection(id, br.you.available_dice, Skill.TargetAffiliation.SELF)
+
+func opponent_dice_selection(id : int, br : BattleRefs):
+	handle_dice_selection(id, br.you.available_dice, Skill.TargetAffiliation.OPPONENT)
 
 func handle_dice_selection(id : int, target_dice : Array[dice], target_affiliation : Skill.TargetAffiliation):
 	if selected_skill == null : return
-	if selected_skill.target_affiliation != target_affiliation : return
+	if selected_skill.target_affiliation != target_affiliation or selected_skill.target_affiliation == Skill.TargetAffiliation.BOTH : return
 	
 	selection_pop_up_confirm.disabled = selected_dice.size() < selected_skill.target_amount
 	
@@ -149,4 +156,4 @@ func handle_dice_selection(id : int, target_dice : Array[dice], target_affiliati
 		selected_dice.append(die)
 	
 	selection_pop_up_confirm.disabled = selected_dice.size() < selected_skill.target_amount
-	update_reminder()
+	update_reminder_text()
